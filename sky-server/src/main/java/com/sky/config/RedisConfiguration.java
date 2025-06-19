368cc2891a6db7e9899019a5e2047add5533a201
wuzy <joeyredfield615@gmail.com>
2025-06-20 00:40:34 +0800

    redis使用JSON序列化，方便在可视化工具查看redis值

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -12,13 +13,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfiguration {
 
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
         log.info("开始创建redis模板对象...");
-        RedisTemplate redisTemplate = new RedisTemplate();
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         //设置redis的连接工厂对象
         redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 使用 JSON 序列化
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+
         //设置redis key的序列化器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(serializer);
+
+        // 如果你是使用 Spring Boot 默认的 RedisAutoConfiguration，它会自动帮你调用这个方法。
+        // 但当你自定义 RedisTemplate 的 Bean（比如为了修改序列化器），就需要手动调用它。
+        redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
+
 }
